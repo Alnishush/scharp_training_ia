005e021fde32c577a7d0f1a6523c2e6af9315e08
Alnishush <aimirizi404@gmail.com>
2025-08-07 21:06:24 +0400

    Задание №4: Сделать всё то же самое, но для контактов

---
+++ addressbook-web-tests_ai/addressbook-web-tests_ai/AddressData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class AddressData
+    {
+        private string firstname;
+        private string lastname;
+
+        public AddressData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+    }
+}
