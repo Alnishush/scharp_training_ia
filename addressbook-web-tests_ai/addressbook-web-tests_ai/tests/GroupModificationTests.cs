8a12a2cfc031c572f16434502452c23d862cc348
Alnishush <aimirizi404@gmail.com>
2025-08-21 20:36:16 +0400

    Исправлен после замечаний. Задание №8: повысить "интеллектуальность" тестов 2

--- addressbook-web-tests_ai/addressbook-web-tests_ai/tests/GroupModificationTests.cs
+++ addressbook-web-tests_ai/addressbook-web-tests_ai/tests/GroupModificationTests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace WebAddressbookTests
 {
